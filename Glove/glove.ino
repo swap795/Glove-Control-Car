0ba32d63d3e725b614b59cc98e95bfb5100ed094
Swap <50679258+swap795@users.noreply.github.com>
2020-11-14 13:34:36 -0700

    Update glove.ino

--- Glove/glove.ino
+++ Glove/glove.ino
@@ -1,1 +1,37 @@
+//for glove
 
+#include <SoftwareSerial.h>
+#include <GY6050.h>
+#include <Wire.h>
+             
+ 
+int X = 0;
+int Y=0;
+GY6050 gyro(0x68);
+SoftwareSerial BTserial(10, 11);
+
+
+void setup() {
+  Serial.begin(9600);
+  BTserial.begin(38400);
+  
+  Wire.begin();
+  gyro.initialisation();
+  delay(2500);
+}
+
+void loop() {
+  X = gyro.refresh('A', 'X');
+  Y= gyro.refresh('A', 'Y');
+  
+  if (X > -10 && X < 10 && Y > -10 && Y < 10)   { BTserial.write('S'); }  
+  if (X < -50 && Y > -10 && Y < 10)             { BTserial.write('F'); }  // Forward
+  if (X > 50 && Y > -10 && Y < 10)              { BTserial.write('B'); }  // Backward
+  if (Y < -50 && X > -10 && X < 10)             { BTserial.write('R'); }  // Right
+  if (Y > 50 && X > -10 && X < 10)              { BTserial.write('L'); }  // Left
+  if (Y < -50 && X < -30)                       { BTserial.write('N'); }   // Right + Forward 
+  if (Y < -50 && X > 30)                        { BTserial.write('M'); }   // R + B
+  if (Y > 50 && X < -30)                        { BTserial.write('O'); }   // L + F 
+  if (Y > 50 && X > 40)                         { BTserial.write('P'); }   // L + B 
+}
+ 
