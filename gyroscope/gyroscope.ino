dcf57cad4b20942c98fff03dd5fbc5dd5e471277
Swap <50679258+swap795@users.noreply.github.com>
2020-11-14 13:25:32 -0700

    This is using the MPU6050 gyroscope

--- gyroscope/gyroscope.ino
+++ gyroscope/gyroscope.ino
@@ -1,52 +1,54 @@
-#include <Wire.h> //I2C Arduino Library
-#define address 0x1E //0011110b, I2C 7bit address of HMC5883
+/*
+  This sketch allows you to read raw data
+  from your MPU6050 multifunctionnal
+  accelerometer, compute it and write
+  them to the Serial monitor.
 
-void setup() {
-  // put your setup code here, to run once:
+  Connect "SDA" from your accelerometer to
+  analog pin 4 of the Arduino (other than Mega),
+  or where it's marked as "SDA" (Rev.3 of Arduino boards).
+  Connect "SDCL" from your accelerometer to
+  analog pin 5 of the Arduino (other than Mega),
+  or where it's marked as "SCL" (Rev.3 of Arduino boards).
 
-  //Initialize Serial and I2C communications
+  Sketch & Library written
+  by Frederic Druppel
+*/
+
+
+
+#include <Wire.h>
+#include <GY6050.h>
+
+
+GY6050 gyro(0x68);                            // Create Gyro object with "0x68" as I2C addres (most likely the address of your MPU6050)
+
+
+void setup()                                  // Main setup
+{
+  gyro.initialisation();
   Serial.begin(9600);
-  Wire.begin();
-
-  //Put the HMC5883 IC into the correct operating mode
-  Wire.beginTransmission(address); //open communication with HMC5883
-  Wire.write(0x02); //select mode register
-  Wire.write(0x00); //continuous measurement mode
-  Wire.endTransmission();
-
+  Serial.println("MPU6050 Test");
+  delay(2500);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-
-  int x, y, z; //triple axis data
-
-  //Tell the HMC5883L where to begin reading data
-  Wire.beginTransmission(address);
-  Wire.write(0x03); //select register 3, X MSB register
-  Wire.endTransmission();
-
-
-  //Read data from each axis, 2 registers per axis
-  Wire.requestFrom(address, 6);
-  if (6 <= Wire.available()) {
-    x = Wire.read() << 8; //X msb
-    x |= Wire.read(); //X lsb
-    z = Wire.read() << 8; //Z msb
-    z |= Wire.read(); //Z lsb
-    y = Wire.read() << 8; //Y msb
-    y |= Wire.read(); //Y lsb
-  }
-
-  //Print out values of each axis
-  Serial.print("x: ");
-  Serial.print(x);
-  Serial.print("  y: ");
-  Serial.print(y);
-  Serial.print("  z: ");
-  Serial.println(z);
-
-  delay(250);
-
+void loop()                                  // Main loop
+{
+  Serial.print("AcX = ");
+  Serial.print(gyro.refresh('A', 'X'));           // Ask for the X axis of the Accelerometer and print it
+  Serial.print(" | AcY = ");
+  Serial.print(gyro.refresh('A', 'Y'));           // Ask for the Y axis of the Accelerometer and print it
+  Serial.print(" | AcZ = ");
+  Serial.print(gyro.refresh('A', 'Z'));           // Ask for the Z axis of the Accelerometer and print it
+  Serial.print(" | Tmp = ");
+  Serial.print(gyro.refresh('T', 'C'));           // Ask for the Temperature in Celsius and print it
+  Serial.print(" C | ");
+  Serial.print(gyro.refresh('T', 'F'));           // Ask for the Temperature in Farenheit and print it
+  Serial.print(" F");
+  Serial.print(" | GyX = ");
+  Serial.print(gyro.refresh('G', 'X'));           // Ask for the X axis of the Gyroscope and print it
+  Serial.print(" | GyY = ");
+  Serial.print(gyro.refresh('G', 'Y'));           // Ask for the Y axis of the Gyroscope and print it
+  Serial.print(" | GyZ = ");
+  Serial.println(gyro.refresh('G', 'Z'));         // Ask for the Z axis of the Gyroscope and print it, then carriage return
+  delay(25);
 }
