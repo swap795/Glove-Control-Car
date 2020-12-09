fcc03581908d58245c3de849ebc3d46113c17c9e
Swap <50679258+swap795@users.noreply.github.com>
2020-12-09 02:22:00 -0700

    Update car.ino

--- Car/car.ino
+++ Car/car.ino
@@ -1,123 +1,164 @@
 #include <SoftwareSerial.h>
-SoftwareSerial BTserial(10, 11);
 
-SoftwareSerial BluetoothSerial(2, 3); // RX | TX
-
+// Enables for the L293d
 #define ENABLE_1 10
 #define ENABLE_2 11
 
-#define DIRA 8
+// left motor
+#define DIRA 8    
 #define DIRB 9
+
+// right motor
 #define DIRC 12
 #define DIRD 13
 
+// HC-SR04 ultra-sonic sensor
+#include "SR04.h"
+#define TRIG_PIN 2
+#define ECHO_PIN 3
+#define led 4
+
+SoftwareSerial BTserial(10, 11);
+
 int state = 0;
+
+// sensor
+SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+long distance;
+
+
+SoftwareSerial BluetoothSerial(7, 6);
+
 
 void setup() {
   Serial.begin(9600);
-  BTserial.begin(38400);
-
+ 
+  //---set pin direction
   pinMode(ENABLE_1, OUTPUT);
   pinMode(ENABLE_2, OUTPUT);
+ 
   pinMode(DIRA, OUTPUT);
   pinMode(DIRB, OUTPUT);
   pinMode(DIRC, OUTPUT);
   pinMode(DIRD, OUTPUT);
 
-//  bluetooth
-  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+
   Serial.println("Enter AT commands:");
- 
-  // HC-06 default baud rate is 9600
-  BluetoothSerial.begin(9600);  
+  BluetoothSerial.begin(9600);
 }
 
 void loop() {
-  if (BluetoothSerial.available() > 0) { 
+  Serial.println("Accessing Sensor ..");
+//  distance = sr04.Distance();
+//  Serial.print(distance);
+    sensor();
+ 
+  if (BluetoothSerial.available() > 0) {
     state = BluetoothSerial.read();
-    
-    Stop();
-    
     switch(state) {
-      case 'F':  
+      case 'S':
+        Stop();
+        break;
+      case 'F':
         forward();
         break;
-      case 'B':  
+      case 'B':
         back();
-        break; 
-      case 'L':  
+        break;
+      case 'L':
         left();
         break;
       case 'R':
         right();
         break;
-      case 'O':  
+      case 'O':
         Fleft();
         break;
       case 'N':
         Fright();
         break;
-      case 'P':  
+      case 'P':
         Bleft();
         break;
       case 'M':
         Bright();
-        break;
+        break;  
     }
-  } 
+  }
 
-
-  /*
-   * Bluetooth
-   */
-  // Keep reading from HC-06 and send to Arduino Serial Monitor
-  if (BluetoothSerial.available()) {
+ 
+  if(BluetoothSerial.available()){
     Serial.write(BluetoothSerial.read());
   }
-      
-  // Keep reading from Arduino Serial Monitor and send to HC-06
-  if (Serial.available()) {
+ 
+  if(BluetoothSerial.available()){
     BluetoothSerial.write(Serial.read());
   }
-
 }
 
-
-  
 void forward() {
-//  analogWrite(E1, 153); // Run in half speed
-//  analogWrite(E2, 255); // Run in full speed
-  
-  digitalWrite(DIRA,HIGH);
-  digitalWrite(DIRB,LOW);
+//  sensor();
+  analogWrite(ENABLE_1, 255); // enable on
+  analogWrite(ENABLE_2, 125); // enable on
+ 
+  // turn the right motor
+  digitalWrite(DIRA, HIGH); //one way
+  digitalWrite(DIRB, LOW);
+
+  // turn the left motor
+  digitalWrite(DIRC, HIGH);  //one way
+  digitalWrite(DIRD, LOW);
 }
 
 void back() {
-  digitalWrite(DIRA,LOW);
+//  sensor();
+  analogWrite(ENABLE_1, 255); // enable on
+  analogWrite(ENABLE_2, 125); // enable on
+
+  // turn the right motor
+  digitalWrite(DIRA,LOW); //reverse
   digitalWrite(DIRB,HIGH);
+
+  // turn the left motor
+  digitalWrite(DIRC, LOW);  //reverse
+  digitalWrite(DIRD, HIGH);
 }
 
 void left() {
-  digitalWrite(DIRC,LOW);
-  digitalWrite(DIRD,HIGH); 
+//  sensor();
+  analogWrite(ENABLE_1, 255); // enable on
+
+  // turn the right motor
+  digitalWrite(DIRA, HIGH); //one way
+  digitalWrite(DIRB, LOW);
 }
 
 void right() {
-  digitalWrite(DIRD,LOW);
-  digitalWrite(DIRC,HIGH);
+//  sensor();
+  analogWrite(ENABLE_2, 125); // enable on
+
+  // turn the left motor
+  digitalWrite(DIRC, HIGH);  //one way
+  digitalWrite(DIRD, LOW);
 }
 
 void Stop() {
-  digitalWrite(DIRA,LOW);
-  digitalWrite(DIRB,LOW);
-  digitalWrite(DIRC,LOW);
-  digitalWrite(DIRD,LOW);
+//  sensor();
+//  digitalWrite(ENABLE_1, LOW);
+//  digitalWrite(ENABLE_2, LOW);
+  analogWrite(ENABLE_1, 0); // enable on
+  analogWrite(ENABLE_2, 0); // enable on
+ 
+  digitalWrite(DIRA, LOW);
+  digitalWrite(DIRB, LOW);
+  digitalWrite(DIRC, LOW);
+  digitalWrite(DIRD, LOW);
 }
 
 void Fright() {
   right();
   forward();
-  
 }
 
 void Fleft() {
@@ -133,4 +174,12 @@ void Bleft() {
 void Bright() {
   right();
   back();
+}
+
+void sensor() {
+  distance = sr04.Distance();
+  digitalWrite(led, LOW);
+  if(distance < 30) {
+    digitalWrite(led, HIGH);
+  }
 }
